e45a030ce8589bc4e906cf90cc12cf15ff7144c2
ayaka <ayaka.murakami1204@gmail.com>
2023-11-14 22:06:55 +0900

    Java19-4

--- src/jp/co/diworks/action/Main.java
+++ src/jp/co/diworks/action/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println(gokei2(2,3));
+		System.out.println(gokei2(5,7));
 	}
 	public static int gokei2(int number1,int number2) {
 		return number1+number2;
